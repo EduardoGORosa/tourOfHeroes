803fbf115c98ca33ea72d3da99556f4f73d26309
Eduardo Rosa <eduardo.g.o.rosa@gmail.com>
2022-04-14 19:31:52 -0300

    correção

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,7 +14,7 @@ export class HeroesComponent implements OnInit{
   constructor(private heroService: HeroService){}
 
   ngOnInit(): void {
-    this.getHeroes;
+    this.getHeroes();
   }
 
   getHeroes(): void{
